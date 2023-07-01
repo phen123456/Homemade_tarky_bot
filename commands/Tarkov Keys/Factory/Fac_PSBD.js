bbce3324b2461569c4fbe98bd5a6a9b1835fc1aa
phen123456 <paulhenling@gmail.com>
2023-07-01 16:23:02 -0400

    Added first key command, still need to work on sub commands

--- commands/Tarkov Keys/Factory/Fac_PSBD.js
+++ commands/Tarkov Keys/Factory/Fac_PSBD.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('discord.js'); //class to allow for / co
 
 module.exports = {
     data: new SlashCommandBuilder() //provide command defintion shown above for registering to Discord
-        .setName('ping')
-        .setDescription('Replies with Pong!'),
+        .setName('pumpingstationbackdoorkey') //no capitals no spaces
+        .setDescription('Replies with Pumping Station Back Door Key statistics'),
     async execute(interaction) {    //Contain the functionality to run from our event handler when the command is used
-        await interaction.reply('Pong!');
+        await interaction.reply('an old soviet factory key labeled "Pumping Station". optional key for Sanitary Standards - Part 1');
     },
 };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Hello wordl
+//Hello wordl 
 
 const fs = require('node:fs');
 const path = require('node:path');
@@ -13,21 +13,35 @@ const client = new Client({
     ]
 })
 
+
+
 client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-const commandsPath = path.join(__dirname, 'commands');  //dynamically retrieve command files
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const subFolders = fs.readdirSync(commandsPath);
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	for (const subFolder of subFolders) {
+		const subFolderPath = path.join(commandsPath, subFolder);
+		const commandFiles = fs.readdirSync(subFolderPath).filter(file => file.endsWith('.js'));
+
+		for (const file of commandFiles) {
+			const filePath = path.join(subFolderPath, file);
+			const command = require(filePath);
+
+			if ('data' in command && 'execute' in command) {
+				client.commands.set(command.data.name, command);
+			} else {
+				console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+			}
+		}
 	}
 }
+
+
+
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
@@ -39,9 +53,26 @@ client.on(Events.InteractionCreate, interaction => {    //Create listener
 	console.log(interaction);
 });
 
-client.on(Events.InteractionCreate, interaction => { //Exit handler if not slash command
+client.on(Events.InteractionCreate, async interaction => { //Exit handler if not slash command
 	if (!interaction.isChatInputCommand()) return;
-	console.log(interaction);
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
 });
 
 client.login(process.env.TOKEN)  //Token for auth
