9782027ad540321a4a06827695ba9d7be2e1147d
phen123456 <paulhenling@gmail.com>
2023-07-01 14:35:53 -0400

    Started on / commands

---
+++ commands/ping.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js'); //class to allow for / commands
+
+module.exports = {
+    data: new SlashCommandBuilder() //provide command defintion shown above for registering to Discord
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
+    async execute(interaction) {    //Contain the functionality to run from our event handler when the command is used
+        await interaction.reply('Pong!');
+    },
+}
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js')
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
 require('dotenv/config')
 
 const client = new Client({
@@ -9,16 +11,35 @@ const client = new Client({
     ]
 })
 
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');  //dynamically retrieve command files
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on('messageCreate', c => {
-    if (c.content === 'ping') {
-        c.reply('pong')
-    }
+client.on(Events.InteractionCreate, interaction => {    //
+	console.log(interaction);
 });
 
-client.login(process.env.TOKEN)
+client.on(Events.InteractionCreate, interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	console.log(interaction);
+});
+
+client.login(process.env.TOKEN)  //Token for auth
