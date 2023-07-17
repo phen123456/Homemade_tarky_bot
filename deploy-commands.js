e5fa166c09a3fd4f9d646fc1e18b864497c30fe9
steeze <ofh26@drexel.edu>
2023-07-16 20:39:42 -0400

    Fixed bugs

--- deploy-commands.js
+++ deploy-commands.js
@@ -36,7 +36,7 @@ const rest = new REST().setToken(process.env.TOKEN); //(process.env.TOKEN) inste
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands((process.env.CLIENTID), (process.env.GUILDID)),
+			Routes.applicationCommands((process.env.CLIENTID)),
 			{ body: commands },
 		);
 
